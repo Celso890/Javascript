7a9460ca6a21bdcc0da3349c57c34213ef8cfafb
Celso Marques Júnior <celsomarques418@gmail.com>
2025-05-27 20:48:56 -0300

    exerciciojava

--- Ex012/Ex015/script.js
+++ Ex012/Ex015/script.js
@@ -1,9 +1,22 @@
-var date = new Date()
-var hora = date.getHours()
-var msg = document.getElementById('msg')
 
-msg.innerHTML = `Agora são ${hora} horas!`
 
-if (hora < 12 ) {
-    
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var date = new Date()
+    var hora = date.getHours()
+    msg.innerHTML = `Agora são ${hora} horas!`
+
+    if (hora > 6 && hora < 12 ) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#FEC86C'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#E5615C'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#5E4475'
+    }
 }
+
+
