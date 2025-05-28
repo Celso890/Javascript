3d2fbcc000aeb28896c76083655791dcdc805770
Celso Marques Júnior <celsomarques418@gmail.com>
2025-05-28 20:46:34 -0300

    estrutura de repetição

---
+++ Ex017/ambiente.js
@@ -0,0 +1,8 @@
+
+
+/*Estrutura de repetição com teste lógico no início*/ 
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
---
+++ Ex017/ambiente2.js
@@ -0,0 +1,7 @@
+
+/*Estrutura de repetição com teste lógico no final*/ 
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
