49e54f7244bcd3d263ad5e544fc50d11e5c17cc6
Celso Marques Júnior <celsomarques418@gmail.com>
2025-05-31 19:28:28 -0300

    funções

---
+++ Ex021/funcao.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n% 2 == 0) {
+        return console.log(`o número ${n} é par!`)
+    } else {
+        return console.log(`O número ${n} é ímpar!`)
+    }
+}
+
+var numero = parimpar(11)
---
+++ Ex021/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1 = 0, n2 = 0) { // caso não seja passado o valor no parâmetro, será o valor padrão
+    return n1 + n2
+}
+
+console.log(soma(2,5))
