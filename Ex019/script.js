4523f5b494577cf028472ff8d9c9a761d1ed356a
Celso Marques Júnior <celsomarques418@gmail.com>
2025-05-31 17:55:29 -0300

    exercício tabuada

--- Ex019/script.js
+++ Ex019/script.js
@@ -1,9 +1,17 @@
-var date = new Date()
-var hora = date.getHours()
-var msg = document.getElementById('msg')
-
-msg.innerHTML = `Agora são ${hora} horas!`
-
-if (hora < 12 ) {
+function gerar(){
+    var num = document.getElementById('inumero')
+    var res = document.getElementById('itabuada')
+    var txt = document.getElementById('itab')
     
+    //txt.innerHTML = 'Digite um número'
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    } else {
+        var numero = Number(num.value)
+        res.innerHTML = ''      
+        for (var i = 1; i <= 10; i++) {
+            var produto = numero * i
+            res.innerHTML += `<option value = "valor${i}"> ${numero} x ${i} = ${produto}</option>` //poderia usar o document.createElement('option')
+        }
+    }
 }
